736de5f6e524e6e129e36e4849ff6b68fe718579
anshumjain <anshumjain1997@gmail.com>
2025-09-19 15:33:20 -0500

    Create nodeSupabaseClient.ts

---
+++ scripts/nodeSupabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import 'dotenv/config';
+
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL || "https://hhdcequsdmosxzjebdyj.supabase.co";
+const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoZGNlcXVzZG1vc3h6amViZHlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNjQwODMsImV4cCI6MjA3MzY0MDA4M30.BJ8tbA2zBC_IgC3Li_uE5P1-cPHA1Gi6mESaJVToPqA";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
